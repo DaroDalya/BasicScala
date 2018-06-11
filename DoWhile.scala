c9b32c406c585f42a742860a87a9a8c94887f9b0
dalya122899 <darodalya17@kit.edu.kh>
2018-06-11 11:34:10 +0700

    cofrjgnjef

---
+++ DoWhile.scala
@@ -0,0 +1,18 @@
+package Looping
+
+object DoWhile 
+{
+  def main (args: Array[String]): Unit=
+  {
+    var a = 20
+    do
+    {
+      println(a)
+      a= a-2 //decrement
+    }
+    while(a!=0) //condition
+    {
+      
+    }
+  }
+}
