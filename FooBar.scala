82ca95c391bd5a7d22f372459846dd85b0c72033
dalya122899 <darodalya17@kit.edu.kh>
2018-06-11 11:30:18 +0700

    first commit

---
+++ FooBar.scala
@@ -0,0 +1,26 @@
+object FooBar 
+{
+   def main(a: Array[String]): Unit = 
+   {
+     var a : Integer = null;
+     for (a <- 1 to 100)
+     {
+       if(a%15==0)
+       {
+         println("foobar");
+       }
+       else if(a%3==0)
+        {
+          println("foo");
+        }
+       else if(a%5==0)
+       {
+         println("bar");
+       }
+       else
+       {
+         println(a);
+       }
+     }  
+   }
+}
